fda1f27ddb06ce24548459bd4873c675005ebdcc
Bartosz Czyzewski <bart94946@gmail.com>
2021-08-12 17:00:18 +0200

    Logical operators

--- test.js
+++ test.js
@@ -1,11 +1,9 @@
-var myAge = 49;
+var myAge =25;
 
-if(myAge > 30){
-    document.write("you are over 30!");
-} else if (myAge > 200) {
-    document.write("you are over 20!")
-} else if (myAge > 10) {
-    document.write("you are over 10!");
-} else (myAge > 10) {
-    document.write("you are not over 10!");
-}
+    if(myAge < 18 || my age > 30 || myAge === 25){
+
+        document.write("You aint coming!")
+
+    } else{
+        document.write("YOu can come, you cool dude")
+    }
