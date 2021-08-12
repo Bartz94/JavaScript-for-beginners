ac295922fb1b1cadcea8f8351f52ac1f4d75f8b3
Bartosz Czyzewski <bart94946@gmail.com>
2021-08-12 16:20:07 +0200

    If and else if statement in JS

--- test.js
+++ test.js
@@ -1,7 +1,11 @@
-var myVariableBart = 10;
+var myAge = 49;
 
-myvar2 = "hello";
-
-myvar2= "hello again"
-
-var customerAge = 32;
+if(myAge > 30){
+    document.write("you are over 30!");
+} else if (myAge > 200) {
+    document.write("you are over 20!")
+} else if (myAge > 10) {
+    document.write("you are over 10!");
+} else (myAge > 10) {
+    document.write("you are not over 10!");
+}
