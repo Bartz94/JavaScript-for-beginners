c71f35c3044ade0452dfdd57cefcea832be41951
Bartosz Czyzewski <bart94946@gmail.com>
2021-08-13 13:21:04 +0200

    This - keyword

--- test.js
+++ test.js
@@ -1,19 +1,13 @@
-var myArray = new Array();
-myArray[0] = 8;
-myArray[1] = "hello"
-
-var myCar = new Object();
-myCar.maxSpeed = 50;
-myCar.driver = "Bart";
-myCar.drive = function () { console.log("now driving"); };
-
-myCar.drive();
 
 var myCar2 = {
     maxSpeed: 70, 
     driver: "Czyzio", 
-    drive: function(speed, time) {console.log(speed * time);}
+    drive: function(speed, time) {console.log(speed * time);},
+    logDriver: function(){
+        console.log("driver name is " + this.driver);
+    }
 };
 
+myCar2.logDriver();
 console.log(myCar2.maxSpeed);
 myCar2.drive(50, 3);
