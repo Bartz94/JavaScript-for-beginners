7bf4deab5bbbc42197825dfa879543a0faa1bc6a
Bartosz Czyzewski <bart94946@gmail.com>
2021-08-13 12:09:41 +0200

    Arrays

--- test.js
+++ test.js
@@ -1,6 +1,18 @@
-var str = "hello, world";
-var str2 = str.slice(2, 9);
-var str3 = str.slice(2);
+var myArray = [];
+myArray[0] = 25;
+myArray[1] = 35;
+myArray[2] = true;
+myArray[3] = "hello";
+console.log(myArray);
+myArray[2] = false;
+console.log(myArray);
 
-var tags = "meat, ham, salami, pork, beef, chicken";
-var tagsArray = tags.split(",");
+var myArray2 = [10, 20 "hi", false];
+console.log(myArray2);
+
+var myArray3 = new Array;
+var myArray3 = new Array(5);
+
+myArray2.length;
+myArray2.sort;
+myArray2.reverse;
