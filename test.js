ea9d0b1136c33964d44434e2a911dc274a56bb50
Bartosz Czyzewski <bart94946@gmail.com>
2021-08-13 18:36:00 +0200

    Removing elements from the dom

--- test.js
+++ test.js
@@ -1,14 +1,9 @@
-var newLi = document.createElement("li");
-var newA = document.createElement("a");
+var parent = document.getElementById("main-nav").getElementsByTagName("ul")[0];
+parent;
 
-var menu = document.getElementById("main-nav").getElementsByTagName("ul")[0];
+var child = parent.getElementsByTagName("li")[0];
+child;
 
+var removed = parent.removeChild(child);
 
-
-menu.appendChild(newLi);
-newLi.appendChild(newA);
-
-newA.innerHTML = "Blog";  //Dodaje napis do elemntuu
-
-
-menu.insertBefore(newLi, menu.getElementsByTagName("li")[0]);     //Dodaje nowy elemnt na koncu 
+parent.appendChild(removed);    //przywroci li tag ponownie do ul tag
