c257755dd73070b9753b084b26e9f6ca630a2bd6
Bartosz Czyzewski <bart94946@gmail.com>
2021-08-13 11:46:55 +0200

    Splitting and slicing strings

--- test.js
+++ test.js
@@ -1,9 +1,6 @@
-var a = "hello";
-var b = 5;
+var str = "hello, world";
+var str2 = str.slice(2, 9);
+var str3 = str.slice(2);
 
-//double negative
-if (!isNaN(a)){
-
-console.log("meaning of life is " + (a * b));
-
-}   
+var tags = "meat, ham, salami, pork, beef, chicken";
+var tagsArray = tags.split(",");
