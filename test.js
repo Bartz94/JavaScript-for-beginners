b96579fb24274bfe26659ae51c45a64923dde652
Bartosz Czyzewski <bart94946@gmail.com>
2021-08-17 17:40:10 +0200

    setInterval and clearInterval

--- test.js
+++ test.js
@@ -1,9 +1,21 @@
-var myMessage = document.getElementById("message");
+var ColorChanger = document.getElementById("color-changer");
+var colors = ["red", "blue", "green", "pink"];
+var counter = 0;
 
-function showMessage(){
+function changeColor(){
 
-    myMessage.className = "show";
+    if (counter >= colors.length){
+        counter = 0;
+    }
 
+    ColorChanger.style.background = colors[counter];
+    counter++;
 }
 
-setTimeout(showMessage, 3000);
+var myTimer = setInterval(changeColor, 2000);
+
+ColorChanger.onclick = function(){
+
+    clearInterval(myTimer);
+    ColorChanger.innerHTML = "Timer stopped";
+};
